e064e02eca77b24c40bb3138daf630d52fc31b6a
ericweidman <ericweidman@gmail.com>
2016-03-28 20:49:13 -0400

    Initial import

---
+++ test/test.py
@@ -0,0 +1,14 @@
+first_name = "Eric"
+last_name = "Weidman"
+age = 28
+
+print "Hello, world!"
+print "My name is " + first_name +" " + last_name + ". " "I am " + str(age) + " " + "years old!"
+print "Python is neat!"
+
+#This is a comment.
+
+"""This is a comment too.
+So is this.
+Also this."""
+
